d568e79b3e2f5a75ea410d77f099a4987adff434
= <parshvashah414@gmail.com>
2019-11-12 01:21:21 -0500

    Add Boundary Class

--- Sokoban.py
+++ Sokoban.py
@@ -12,25 +12,35 @@ def get_sprite(sprite_name):
     return image
 
 class Player(pygame.sprite.Sprite):
- 
+
     #Construct the player class
     def __init__(self, x, y):
         super().__init__()
- 
+
         # TODO: CHANGE BASE RECTANGLE CHARACTER TO SPRITE
         self.image = get_sprite("character.png")
-        
- 
+
+
         # Set the top left position of the location to the x,y coordinate passed in
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
 
- 
+
     def update(self):
         """Used to update the players position"""
         pass
 
+class Box(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        this.image = pygame.image.load("Sprites/brick.jpg")
+        this.x = x
+        this.y = y
+    def blit():
+        screen.blit(this.image,x, y )
+
+
+        
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Test")
@@ -49,9 +59,9 @@ while gameRunning:
             gameRunning = False
 
     sprites_list.update()
-    sprites_list.draw(screen) 
-    pygame.display.flip() 
+    sprites_list.draw(screen)
+    pygame.display.flip()
     clock.tick(60)
-    
+
 pygame.quit()
 quit()
